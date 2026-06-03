5402946b27ed10a56ae69f52bdbf2a7a5de89d90
Rafael S Lima <rafaelkra2@gmail.com>
2026-06-02 22:51:19 -0300

    Update metrics handler to return HTML response with visit count

--- metrics.go
+++ metrics.go
@@ -6,9 +6,14 @@ import (
 )
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	text := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	text := fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())
 	w.Write([]byte(text))
 }
 
